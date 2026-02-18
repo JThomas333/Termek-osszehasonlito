d95d604e1b2c21b8cb921d0dca1bb8b368e22181
Tamás Józsa <c0joztam@vasvari.org>
2026-02-18 14:08:31 +0100

    main_app_json

--- Termek_Osszehasonlito/src/main.js
+++ Termek_Osszehasonlito/src/main.js
@@ -1,4 +1,6 @@
-import './assets/main.css'
+import Vue3Toastify from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -10,5 +12,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Vue3Toastify, {
+    autoclose : 2000,
+    position: "top-right",
+    theme : 'colored'
+})
 
 app.mount('#app')
